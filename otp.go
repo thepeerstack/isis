e96ad5b331522b9ab5f8f38c22ab3160430558dc
Michael Okoh <ordrizzy@gmail.com>
2023-01-22 22:35:15 +0000

    OTP model

---
+++ otp.go
@@ -0,0 +1,29 @@
+package isis
+
+import "time"
+
+type otp struct {
+	Id         int64
+	Identifier string
+	Token      string
+	Validity   int
+	Valid      bool
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
+
+func (otp otp) Empty() bool {
+	if otp.Id == 0 {
+		return true
+	}
+
+	return false
+}
+
+func (otp otp) Exipired() bool {
+	if time.Now().Sub(otp.CreatedAt).Minutes() > float64(otp.Validity) {
+		return true
+	}
+
+	return false
+}
